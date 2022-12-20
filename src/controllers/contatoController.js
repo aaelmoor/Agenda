fbcf63cb9d47dc2a82c8a0abcf1f633e3455bf5a
Alexandre Abdu El-moor <alexandre.elmoor@gmail.com>
2022-12-20 16:42:34 -0300

    02 - Após Edita Contato

--- src/controllers/contatoController.js
+++ src/controllers/contatoController.js
@@ -7,6 +7,7 @@ exports.index = (req, res) => {
 
 exports.register = async (req, res) => {
     try {
+        console.log('register');
         const contato = new Contato(req.body);
         await contato.register();
 
@@ -25,9 +26,30 @@ exports.register = async (req, res) => {
     }
 };
 
-exports.editIndex = async function(req, res) {
+exports.editIndex = async function (req, res) {
     if (!req.params.id) return res.render('404');
     const contato = await Contato.buscaPorId(req.params.id);
     if (!contato) return res.render('404');
     res.render('contato', { contato });
+};
+
+exports.edit = async function (req, res) {
+    try {
+        if (!req.params.id) return res.render('404');
+        const contato = new Contato(req.body);
+        await contato.edit(req.params.id);
+
+        if (contato.errors.length > 0) {
+            req.flash('errors', contato.errors);
+            req.session.save(() => res.redirect('back'));
+            return;
+        }
+
+        req.flash('success', 'Contato editado com sucesso.');
+        req.session.save(() => res.redirect(`/contato/index/${contato.contato._id}`));
+        return;
+    } catch (e) {
+        console.log(e);
+        res.render('404');
+    }
 };
