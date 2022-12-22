ee5e3a12e5a56036e4bdf159212ffb205ad7fe8f
Alexandre Abdu El-moor <alexandre.elmoor@gmail.com>
2022-12-22 10:12:20 -0300

    03 - Após Delete Contato

--- src/controllers/contatoController.js
+++ src/controllers/contatoController.js
@@ -7,7 +7,6 @@ exports.index = (req, res) => {
 
 exports.register = async (req, res) => {
     try {
-        console.log('register');
         const contato = new Contato(req.body);
         await contato.register();
 
@@ -52,4 +51,14 @@ exports.edit = async function (req, res) {
         console.log(e);
         res.render('404');
     }
+};
+
+exports.delete = async function (req, res) {
+    if (!req.params.id) return res.render('404');
+    const contato = await Contato.delete(req.params.id);
+    if (!contato) return res.render('404');
+   
+    req.flash('success', 'Contato apagado com sucesso.');
+    req.session.save(() => res.redirect(`back`));
+    return;
 };
